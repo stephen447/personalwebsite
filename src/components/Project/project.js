076c74bde9f8fa6e7a9a47cfafcaf8b5d19819df
stephen447 <byrnes67@tcd.ie>
2024-08-25 22:55:03 +0100

    adding link to projects

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -1,14 +1,24 @@
-import "./project.css"
-import Techstack from "../TechStack/techStack"
-export default function Project (props) {
-    return(
+import "./project.css";
+import Techstack from "../TechStack/techStack";
+
+export default function Project(props) {
+    const { title, year, description, techstack, techStack, url } = props;
+
+    const projectContent = (
         <div className="project__container">
-            <h2 className="project__title">{props.title}</h2>
-            <p className="project__year">{props.year}</p>
-            <p className="project__description">{props.description}</p>
-            <div className="project__techstack">{props.techstack}</div>
-            <Techstack techStack={props.techStack}></Techstack>
+            <h2 className="project__title">{title}</h2>
+            <p className="project__year">{year}</p>
+            <p className="project__description">{description}</p>
+            <div className="project__techstack">{techstack}</div>
+            <Techstack techStack={techStack}></Techstack>
         </div>
-    )
-    
+    );
+
+    return url ? (
+        <a href={url} target="_blank" rel="noopener noreferrer" className="project__link">
+            {projectContent}
+        </a>
+    ) : (
+        projectContent
+    );
 }
